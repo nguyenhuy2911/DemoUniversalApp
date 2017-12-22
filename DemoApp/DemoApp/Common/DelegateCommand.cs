d4912c9562735b0a8bb936ff3ceb00d124cb5ad0
nguyenhuy2911 <huy.nguyen.git@gmail.com>
2017-12-23 01:11:49 +0700

    commit file

---
+++ DemoApp/DemoApp/Common/DelegateCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DemoApp.Common
+{
+   public class DelegateCommand : ICommand
+    {
+        private SimpleEventHandler handler;
+        private bool isEnable = true;
+        public event EventHandler CanExecuteChanged;
+
+        public delegate void SimpleEventHandler();
+        public DelegateCommand(SimpleEventHandler handler)
+        {
+            this.handler = handler;
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return isEnable;
+            }
+        }
+        private void OnCanExcuteChanged()
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+          return  this.IsEnabled;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.handler();
+        }
+    }
+}
---
+++ DemoApp/DemoApp/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using DemoApp.Common;
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+
+namespace DemoApp.ViewModels
+{
+    public class StartPageViewModel : ViewModelBase
+    {
+        private bool _isLoading = false;
+
+        public bool IsLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+            set
+            {
+                _isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        public StartPageViewModel()
+        {
+            Title = "Hello World";
+        }
+
+        public ICommand Changelabel
+        {
+            get
+            {
+                return new DelegateCommand(Change);
+            }
+        }
+
+        public void Change()
+        {
+
+            Title = "Well come to Demo App";
+     
+        }
+
+    }
+}
